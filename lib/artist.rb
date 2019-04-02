67173de50b41b741ea6c4780085e86a1bc1ce67d
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-04-02 16:44:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,37 +1,22 @@
-require "pry"
-
 class Artist
+	attr_accessor :name
+	def initialize(name)
+		@name = name
+		@songs = []
+	end
 
-  @@all = []
+	def songs
+		@songs
+	end
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
+	def add_song(song)
+			@songs << song
+			song.artist = self
+	end
 
-  attr_reader :name
-
-  def self.all
-    @@all
-  end
-
-  def new_song(name, genre)
-    song = Song.new(name, self, genre)
-  end
-
-  def songs
-    Song.all.collect do |song|
-      if song.artist.name == @name
-        song
-      end
-    end
-  end
-
-  def genres
-    output = songs.collect do |song|
-      song.genre
-    end
-    #binding.pry
-  end
-
+	def genres
+		@songs.collect do |x|
+			x.genre
+		end
+	end
 end
